9bb268b0312e9240246143df24dc807b0f6f34f5
VincentOUALLET <vincent.ouallet@yahoo.fr>
2020-05-15 13:28:30 +0200

    initialisation du repo git

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+?>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>APOCALYPARK</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet "href="css/style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <!-- <h1 class="site-heading text-center text-white d-none d-lg-block"> -->
+        <h1 class="site-heading text-center d-none d-lg-block">
+            <span class="site-heading-upper text-primary mb-3">Réservez! Tant que vous le pouvez encore...</span>
+            <span class="site-heading-lower">Apocalypark</span>
+        </h1>
+
+
+    </div>
+
+
+
+</body>
+
+</html>
